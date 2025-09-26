e93cff260b30277f937447f87202c213d6456132
eric-spb <er.fv@icloud.com>
2025-09-26 23:25:26 +0300

    Add files via upload

---
+++ PP_2_2_3/src/main/java/web/dao/PeopleDAO.java
@@ -0,0 +1,47 @@
+package web.dao;
+
+import org.springframework.stereotype.*;
+import web.model.User;
+
+import javax.persistence.*;
+import javax.transaction.*;
+import java.util.*;
+
+@Repository
+@Transactional
+@Component
+public class PeopleDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<User> getPeople() {
+     return em.createQuery("select p from User p", User.class).getResultList();
+    }
+
+
+    public User look(int id) {
+        return em.find(User.class, id);
+    }
+
+
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    public void delete(int id) {
+        em.createQuery("delete from User p where p.id = :id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+
+    public void update(int id, User updatedUser) {
+        User toUpdate = look(id);
+
+        toUpdate.setName(updatedUser.getName());
+        toUpdate.setAge(updatedUser.getAge());
+        toUpdate.setSurname(updatedUser.getSurname());
+
+        em.merge(toUpdate);
+    }
+}
